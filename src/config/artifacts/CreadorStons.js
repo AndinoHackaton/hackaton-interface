986fed940f1ec4763de350ad35c17437d91799a4
Rafael Livise <r.livise@hotmail.com>
2022-10-28 08:04:26 -0500

    feat: add creator contract

---
+++ src/config/artifacts/CreadorStons.js
@@ -0,0 +1,122 @@
+const Artifacts = {
+  address: {
+    97: '0x75275A7466BFBafC5A46319aA27566D1907f895C'//contrato desplegado,
+  },
+  abi: [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "contractAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "projectStarter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "projectTitle",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "goalAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "ProjectStarted",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "getHaveProyect",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "getHaveProyectByAddress",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMyProyect",
+		"outputs": [
+			{
+				"internalType": "contract SwapInstantaneo",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "getProyectByAddress",
+		"outputs": [
+			{
+				"internalType": "contract SwapInstantaneo",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amountToRaise",
+				"type": "uint256"
+			}
+		],
+		"name": "startProject",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+}
+
+export default Artifacts
